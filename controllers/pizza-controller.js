4c04b63af8571f7344d6d687e6eca18649846694
JideOgun <JideOgun@github.com>
2022-03-03 18:12:26 -0600

    added comment functionality

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,5 +1,5 @@
 const { Pizza } = require('../models');
-const { db } = require('../models/Pizza');
+
 
 
 const pizzaController = {
@@ -7,6 +7,12 @@ const pizzaController = {
 // get all pizzas
 getAllPizza(req, res) {
     Pizza.find({})
+    .populate({
+        path: 'comments',
+        select: '-__v'
+    })
+    .select('-__v')
+    .sort({ _id: -1 })
     .then(dbPizzaData => res.json(dbPizzaData))
     .catch(err => {
         console.log(err);
@@ -17,6 +23,11 @@ getAllPizza(req, res) {
 // get one pizza by id
 getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
+    .populate({
+        path: 'comments',
+        select: '-__v'
+    })
+    .select('-__v')
     .then(dbPizzaData => {
         // if no pizza is found, send 404
         if(!dbPizzaData) {
